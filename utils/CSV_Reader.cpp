a49f094f4f373cab34ed71f06fffc05e62557aa3
Sebastian Knell <sebastianknell@hotmail.com>
2021-05-24 12:19:42 -0500

    csv reading library and dataset

--- utils/CSV_Reader.cpp
+++ utils/CSV_Reader.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "CSV_Reader.h"
+
+void CSV_Reader::load() {
+    rapidcsv::Document document(filename);
+    auto len = document.GetRowCount();
+    for (int i = 0; i < len; i++) {
+        vector<string> row = document.GetRow<string>(0);
+        // Save row to record and write to file
+    }
+}
--- utils/CSV_Reader.h
+++ utils/CSV_Reader.h
@@ -5,9 +5,19 @@
 #ifndef DB2_PROJECT1_CSV_READER_H
 #define DB2_PROJECT1_CSV_READER_H
 
+#include <iostream>
+#include <string>
+#include <utility>
+#include <vector>
+#include "rapidcsv.h"
+
+using namespace std;
 
 class CSV_Reader {
-
+    string filename;
+public:
+    explicit CSV_Reader(string filename = "data/stroke-data.csv"): filename(std::move(filename)) {};
+    void load();
 };
 
 
