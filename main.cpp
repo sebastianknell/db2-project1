4d53649708c57cef294989126560459cde1a52ad
Sebastian Knell <sebastianknell@hotmail.com>
2021-06-01 21:32:13 -0500

    merge test in main

--- main.cpp
+++ main.cpp
@@ -5,17 +5,11 @@
 using namespace std;
 
 int main() {
-//    Tester::buffer_test();
-//    Tester::record_test();
     SequentialFile sequentialFile;
-//    sequentialFile.load_data("stroke-data.csv");
-//    sequentialFile.print_all();
-//    auto result = sequentialFile.search(66494);
-//    if (result.has_value()) print_record(result.value());
-
+    sequentialFile.load_data("stroke-data.csv");
     FixedRecord record;
     vector<string> record_data;
-    record_data.push_back("2");
+    record_data.push_back("1");
     record_data.push_back("Male");
     record_data.push_back("25");
     record_data.push_back("1");
@@ -28,10 +22,38 @@ int main() {
     record_data.push_back("smokes");
     record_data.push_back("0");
     record.load_data(record_data);
-//    if (!sequentialFile.insert(record)) cout << "false\n";
-
-    auto result = sequentialFile.search(2);
-    if (result.has_value()) print_record(result.value());
-//    sequentialFile.print_all();
+    if (!sequentialFile.insert(record)) cout << "false\n";
+    record_data[0] = "2";
+    record.load_data(record_data);
+    if (!sequentialFile.insert(record)) cout << "false\n";
+    record_data[0] = "3";
+    record.load_data(record_data);
+    if (!sequentialFile.insert(record)) cout << "false\n";
+    record_data[0] = "4";
+    record.load_data(record_data);
+    if (!sequentialFile.insert(record)) cout << "false\n";
+    record_data[0] = "5";
+    record.load_data(record_data);
+    if (!sequentialFile.insert(record)) cout << "false\n";
+    record_data[0] = "6";
+    record.load_data(record_data);
+    if (!sequentialFile.insert(record)) cout << "false\n";
+    record_data[0] = "7";
+    record.load_data(record_data);
+    if (!sequentialFile.insert(record)) cout << "false\n";
+    record_data[0] = "8";
+    record.load_data(record_data);
+    if (!sequentialFile.insert(record)) cout << "false\n";
+    record_data[0] = "9";
+    record.load_data(record_data);
+    if (!sequentialFile.insert(record)) cout << "false\n";
+    record_data[0] = "10";
+    record.load_data(record_data);
+    if (!sequentialFile.insert(record)) cout << "false\n";
+    record_data[0] = "11";
+    record.load_data(record_data);
+    if (!sequentialFile.insert(record)) cout << "false\n";
+    sequentialFile.print_all();
+    
     return 0;
 }
